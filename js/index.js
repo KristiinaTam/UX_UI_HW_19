d4b08fd8437641357ec30c87c3ea27d321fe8f76
KristiinaTam <kristiinatam@gmail.com>
2024-04-14 13:46:04 -0600

    save

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
-console.log("bananas")
-//i hate this 
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerBtn = document.querySelector('.hamburger');
-    const navMenu = document.querySelector('nav');
-
-    // Toggle navigation menu visibility when hamburger menu button is clicked
-    hamburgerBtn.addEventListener('click', function () {
-        // Toggle hamburger button animation
-        this.classList.toggle('animate');
-        
-        // Toggle navigation menu animation
-        navMenu.classList.toggle('animate');
-    });
-});
+(function($) { // Begin jQuery
+    $(function() { // DOM ready
+      // If a link has a dropdown, add sub menu toggle.
+      $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        // Close one dropdown when selecting another
+        $('.nav-dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+      });
+      // Clicking away from dropdown will remove the dropdown class
+      $('html').click(function() {
+        $('.nav-dropdown').hide();
+      });
+      // Toggle open and close nav styles on click
+      $('#nav-toggle').click(function() {
+        $('nav ul').slideToggle();
+      });
+      // Hamburger to X toggle
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+    }); // end DOM ready
+  })(jQuery); // end jQuery
